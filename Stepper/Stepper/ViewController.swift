09ed606e2e156a9314e3fa469dfb4abff298cce9
stven1004 <stven100482@gmail.com>
2019-05-16 15:24:39 +0900

    add code stepper

--- Stepper/Stepper/ViewController.swift
+++ Stepper/Stepper/ViewController.swift
@@ -10,8 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var valueStepper: UIStepper!
+    @IBOutlet weak var autorepeatSwitch: UISwitch!
+    @IBOutlet weak var continuousSwitch: UISwitch!
+    @IBOutlet weak var wrapSwitch: UISwitch!
+    
+    // switch Action.
+    @IBAction func toggleAutorepeat(_ sender: UISwitch) {
+        valueStepper.autorepeat = sender.isOn
+    }
+    
+    @IBAction func toggleContinuous(_ sender: UISwitch) {
+        valueStepper.isContinuous = sender.isOn
+    }
+    
+    @IBAction func toggleWrap(_ sender: UISwitch) {
+        valueStepper.wraps = sender.isOn
+    }
+    
+    //stepper Action.
+    @IBAction func valueChanged(_ sender: UIStepper) {
+        valueLabel.text = "\(Int(sender.value))"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        autorepeatSwitch.isOn = valueStepper.autorepeat
+        continuousSwitch.isOn = valueStepper.isContinuous
+        wrapSwitch.isOn = valueStepper.wraps
         // Do any additional setup after loading the view.
     }
 
