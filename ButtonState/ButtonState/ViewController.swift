23a75a0846eab9659089d20792eb0dc1b2047c1c
stven1004 <stven100482@gmail.com>
2019-05-17 12:22:28 +0900

    add controlSource

--- ButtonState/ButtonState/ViewController.swift
+++ ButtonState/ButtonState/ViewController.swift
@@ -9,12 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var stateLabel: UILabel!
+    
+    @IBOutlet weak var btn: UIButton!
+    
+    @IBAction func report(_ sender: UIButton) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+            self.stateLabel.text = sender.state.debugString
+        }
+    }
+    
+    @IBAction func stateChanged(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            btn.isEnabled = true
+            btn.isEnabled = false
+            btn.isHighlighted = false
+        case 1:
+            btn.isHighlighted = !btn.isHighlighted
+        case 2:
+            btn.isSelected = !btn.isSelected
+        case 3:
+            btn.isEnabled = false
+        default:
+            break
+        }
+        
+        report(btn)
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        stateLabel.text = btn.state.debugString
     }
 
 
+}
+
+
+extension UIControl.State {
+    var debugString: String{
+        var list = [String]()
+        if contains(.normal) {
+            list.append("Normal")
+        }
+        if contains(.highlighted) {
+            list.append("Highlighted")
+        }
+        if contains(.disabled) {
+            list.append("Disabled")
+        }
+        if contains(.selected) {
+            list.append("Selected")
+        }
+        
+        return list.joined(separator: ", ")
+    }
+    
 }
 
